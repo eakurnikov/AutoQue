bc4071bd9a21818c3cea7dc2a797c60140cd6ae9
eakurnikov <egor.kurnikov@kaspersky.com>
2019-09-20 14:42:36 +0300

    finding view nodes by autofill ids modified

--- feature_autofill_impl/src/main/kotlin/com/eakurnikov/autoque/autofill/impl/extensions/FillContextExt.kt
+++ feature_autofill_impl/src/main/kotlin/com/eakurnikov/autoque/autofill/impl/extensions/FillContextExt.kt
@@ -2,14 +2,47 @@ package com.eakurnikov.autoque.autofill.impl.extensions
 
 import android.app.assist.AssistStructure
 import android.service.autofill.FillContext
+import android.util.SparseIntArray
 import android.view.autofill.AutofillId
+import java.util.LinkedList
 
 /**
  * Created by eakurnikov on 2019-09-15
  */
 
 fun FillContext.findViewNodes(autofillIds: Array<AutofillId>): Array<AssistStructure.ViewNode?> {
-    return javaClass
-        .getMethod("findViewNodesByAutofillIds", Array<AutofillId>::class.java)
-        .invoke(this, autofillIds) as Array<AssistStructure.ViewNode?>
+    val viewNodesToProcess: LinkedList<AssistStructure.ViewNode> = LinkedList()
+
+    val windowNodeCount: Int = structure.windowNodeCount
+    for (i in 0 until windowNodeCount) {
+        viewNodesToProcess.add(
+            structure.getWindowNodeAt(i).rootViewNode
+        )
+    }
+
+    val missingViewNodeIndexes: SparseIntArray =
+        SparseIntArray(autofillIds.size).apply { autofillIds.indices.forEach { put(it, 0) } }
+
+    val foundViewNodes: Array<AssistStructure.ViewNode?> = arrayOfNulls(autofillIds.size)
+
+    while (missingViewNodeIndexes.size() > 0 && !viewNodesToProcess.isEmpty()) {
+        val viewNode: AssistStructure.ViewNode = viewNodesToProcess.removeFirst()
+
+        for (i in 0 until missingViewNodeIndexes.size()) {
+            val viewNodeIndex: Int = missingViewNodeIndexes.keyAt(i)
+            val autofillId: AutofillId = autofillIds[viewNodeIndex]
+
+            if (autofillId == viewNode.autofillId) {
+                foundViewNodes[viewNodeIndex] = viewNode
+                missingViewNodeIndexes.removeAt(i)
+                break
+            }
+        }
+
+        for (i in 0 until viewNode.childCount) {
+            viewNodesToProcess.addLast(viewNode.getChildAt(i))
+        }
+    }
+
+    return foundViewNodes
 }
